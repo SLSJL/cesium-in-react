6865891a2ebb3756f117e5934815ba0be58cbe28
SUN <350916525@qq.com>
2022-09-30 23:01:06 +0800

    feat: added terrain

--- src/routes/StartUp/index.tsx
+++ src/routes/StartUp/index.tsx
@@ -1,23 +1,45 @@
 import React, { useEffect, useRef } from "react";
 import "./startUp.less";
-import { Viewer } from "cesium";
+import { Viewer, CesiumTerrainProvider } from "cesium";
 
 const StartUp = () => {
   const csmViewerRef = useRef<null | Viewer>(null);
   const viewerContainerRef = useRef(null);
 
+  const onLoadingTerrain = () => {
+    if (csmViewerRef.current) {
+      csmViewerRef.current.terrainProvider = new CesiumTerrainProvider({
+        url: 'http://localhost:7000/terrain',
+        requestVertexNormals: true
+      })
+    }
+
+  }
+
   useEffect(() => {
     if (viewerContainerRef.current && !csmViewerRef.current) {
-      csmViewerRef.current = new Viewer("csm-viewer-container");
+      const viewer = new Viewer("csm-viewer-container");
+
+
+      csmViewerRef.current = viewer
     }
   }, [viewerContainerRef]);
 
   return (
-    <div
-      className="csm-viewer-container"
-      id="csm-viewer-container"
-      ref={viewerContainerRef}
-    ></div>
+    <>
+      <div   className="csm-viewer-tools">
+        <button onClick={() => onLoadingTerrain()}>
+          Load terrain
+        </button>
+      </div>
+
+      <div
+        className="csm-viewer-container"
+        id="csm-viewer-container"
+        ref={viewerContainerRef}
+      ></div>
+    </>
+
   );
 };
 
